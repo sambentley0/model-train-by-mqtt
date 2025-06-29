606e1a19a90fa8eff091707e6fdaf1bab2520a76
sambentley0 <116500439+sambentley0@users.noreply.github.com>
2025-06-29 15:04:02 +0300

    Update secrets_template.h

--- secrets_template.h
+++ secrets_template.h
@@ -1,6 +1,7 @@
 #pragma once
 
-const char* mqtt_server = "192.168.1.50";  // replace with Pi's IP
-const int mqtt_port = 1883;
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+const char* mqtt_server = "192.168.1.50";  // your Pi's IP
 const char* mqtt_user = "myuser";
 const char* mqtt_pass = "mypassword";
