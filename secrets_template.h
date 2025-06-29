76437c97721e062b07ba5b40ac1443bb0adcb81a
sambentley0 <116500439+sambentley0@users.noreply.github.com>
2025-06-29 14:57:28 +0300

    Update secrets_template.h
    To use a Raspberry Pi using Mosquitto

--- secrets_template.h
+++ secrets_template.h
@@ -1,5 +1,6 @@
 #pragma once
 
-const char* ssid = "YOUR_WIFI_SSID";
-const char* password = "YOUR_WIFI_PASSWORD";
-const char* mqtt_server = "192.168.1.100";
+const char* mqtt_server = "192.168.1.50";  // replace with Pi's IP
+const int mqtt_port = 1883;
+const char* mqtt_user = "myuser";
+const char* mqtt_pass = "mypassword";
