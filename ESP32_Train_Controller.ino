6367cb6294e15051968c7b18eee478dc8b4fa7d6
sambentley0 <116500439+sambentley0@users.noreply.github.com>
2025-06-29 14:13:36 +0300

    Update ESP32_Train_Controller.ino
    Move wifi details to a secrets file

--- ESP32_Train_Controller.ino
+++ ESP32_Train_Controller.ino
@@ -3,9 +3,7 @@
 #include <Preferences.h>
 
 // WiFi details
-const char* ssid = "YOUR_WIFI_SSID";
-const char* password = "YOUR_WIFI_PASSWORD";
-const char* mqtt_server = "192.168.1.100";
+#include "secrets.h"
 
 // GPIO definitions
 #define IN1 16
